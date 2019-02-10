a8b61dbc6a5d754302801f19ddac7a90ba970280
Xavier Vierling <xvrlng@gmail.com>
2019-02-10 11:17:20 +0100

    - Entité et fixture "actualité" - Ajout dans EasyAdmin

---
+++ src/Entity/Actu.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ActuRepository")
+ */
+class Actu
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_actu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDateActu(): ?\DateTimeInterface
+    {
+        return $this->date_actu;
+    }
+
+    public function setDateActu(\DateTimeInterface $date_actu): self
+    {
+        $this->date_actu = $date_actu;
+
+        return $this;
+    }
+}
