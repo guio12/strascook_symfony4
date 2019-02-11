f6d772699d0c3b95a247072556e9597d8f2f2563
Unknown <g.audouze@gmail.com>
2019-02-11 11:58:22 +0100

    Fixtures TypeMenu et Menu OK
    Loader d'abord le TypeMenyFixtures : php bin/console doctrine:fixtures:load --group=TypeMenuFixtures --append
    
    et ensuite le MenuFixtures : php bin/console doctrine:fixtures:load --group=MenuFixtures --append

--- src/DataFixtures/MenuFixtures.php
+++ src/DataFixtures/MenuFixtures.php
@@ -5,3 +5,45 @@
  * Date: 10/02/2019
  * Time: 13:53
  */
+
+namespace App\DataFixtures;
+
+use App\Entity\Menu;
+use App\Entity\TypeMenu;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\DataFixtures\TypeMenuFixtures;
+
+
+
+class MenuFixtures extends Fixture 
+{
+    public function load(ObjectManager $manager)
+    {
+
+        $em = $manager->getRepository(TypeMenu::class);
+        
+        $typeMenu = $em->findAll();
+       
+        foreach ($typeMenu as $type) {
+        
+        $menu = new Menu();
+        $menu->setType($type);
+        $menu->setTitre("Menu test " . $type);
+        $menu->setIntroduction("Introduction");
+        $menu->setEntree("Nom d'entrée");
+        $menu->setDEntree("Description de l'entrée");
+        $menu->setPlat("Nom du plat");
+        $menu->setDPlat("Description du plat");
+        $menu->setDessert("Nom du dessert");
+        $menu->setDDessert("Decription du dessert");
+        $menu->setPrix(rand(1, 100));
+    
+        $manager->persist($menu);
+        }
+        
+        $manager->flush();
+    }
+        
+        
+}
--- src/DataFixtures/TypeMenuFixtures.php
+++ src/DataFixtures/TypeMenuFixtures.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xavier
- * Date: 10/02/2019
- * Time: 13:26
- */
+
+
+namespace App\DataFixtures;
+
+use App\Entity\Menu;
+use App\Entity\TypeMenu;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\DataFixtures\MenuFixtures;
+
+
+class TypeMenuFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $types = ['vegetarien', 'vegan', 'classique'];
+        
+        foreach($types as $i => $nom)
+        {
+            $type[$i] = new TypeMenu();
+            $type[$i]->setType($nom);
+            
+            $manager->persist($type[$i]);
+        }
+        
+        $manager->flush();
+    }
+    
+}
